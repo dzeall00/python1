48b9515028125333c96060954d924863a083bc45
dzeall00 <dzuevalbert0@gmail.com>
2023-10-30 20:12:41 +0300

    Завершение программы

--- Программа проверяющая лежит ли точка на отрезке/Программа проверяющая лежит ли точка на отрезке/Программа проверяющая лежит ли точка на отрезке.cpp
+++ Программа проверяющая лежит ли точка на отрезке/Программа проверяющая лежит ли точка на отрезке/Программа проверяющая лежит ли точка на отрезке.cpp
@@ -31,3 +31,29 @@ bool thc(double x, double y, double z, double w, double a, double b)
 
     return b == a * k + c;
 }
+
+int main(int argc, char* argv[])
+{
+    setlocale(LC_ALL, "Russian");
+
+    double x, y; // Координаты начала отрезка
+    double z, w; // Координаты конца отрезка
+    double a, b; // Координаты точки
+
+    bool result;
+
+    cout << "Координаты начала отрезка: ";
+    cin >> x >> y;
+
+    cout << "Координаты конца отрезка: ";
+    cin >> z >> w;
+
+    cout << "Координаты точки: ";
+    cin >> a >> b;
+
+    result = thc(x, y, z, w, a, b);
+    cout << result << endl;
+    system("pause");
+
+    return 0;
+}
