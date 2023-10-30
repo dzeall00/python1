8a1081503b080bf3d2e556169259800aef73f9e9
dzeall00 <dzuevalbert0@gmail.com>
2023-10-30 20:08:12 +0300

    введение координат точки и проверка на принадлежность к отрезку

--- Программа проверяющая лежит ли точка на отрезке/Программа проверяющая лежит ли точка на отрезке/Программа проверяющая лежит ли точка на отрезке.cpp
+++ Программа проверяющая лежит ли точка на отрезке/Программа проверяющая лежит ли точка на отрезке/Программа проверяющая лежит ли точка на отрезке.cpp
@@ -18,3 +18,16 @@ double min(double x, double y)
     }
     return x;
 }
+bool thc(double x, double y, double z, double w, double a, double b)
+{
+    double k, c;
+
+    if (z == x) {
+        return (a == x && b >= min(y, w) && x <= max(y, w));
+    }
+
+    k = (w - y) / (z - x);
+    c = y - k * x;
+
+    return b == a * k + c;
+}
